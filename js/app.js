49492e65ff07c2dea06a28784758c15ce5436a91
Benjamin Small <smallbenjamin94@gmail.com>
2022-04-06 12:06:47 -0700

    fixed infinite loop scenario

--- js/app.js
+++ js/app.js
@@ -4,11 +4,11 @@ document.getElementById('name').textContent = username;
 alert('Greetings ' + username + ', welcome to the Benjamin Small immersive experience\r\rIf you can answer these simple questions correct we will know you are a true Benjain Small superfan and will be entered into a raffle to win our grand prize of $100 million!!');
 
 let numberCorrect = 0;
-no(prompt('Was Ben born on a Wednesday?'));
-no(prompt('Has Ben ever worked for Miracle Ram?'));
-yes(prompt('Does Ben want kids?'));
-no(prompt('Is Ben an only child?'));
-yes(prompt('Does Ben have more Nephews than Neices?'));
+no('Was Ben born on a Wednesday?');
+no('Has Ben ever worked for Miracle Ram?');
+yes('Does Ben want kids?');
+no('Is Ben an only child?');
+yes('Does Ben have more Nephews than Neices?');
 if(numberCorrect >= 5) {
   alert('Congatulations ' + username + '! You are a true Benjamin Small superfan! None can compare to your prowess!! I know we promised you $100 million, but please take this complementary "Atta boy"!!');
 } else if (numberCorrect > 0) {
@@ -17,13 +17,12 @@ if(numberCorrect >= 5) {
   alert('Sorry ' + username + ', you did not answer a single question correct. You are an utter disappointment of a person. :(');
 }
 
-function no(response) {
-  if(response === null) {
-    response = '';
-  } else {
-    response = response.toLowerCase();
-    let validResponse = false;
-    while(!validResponse) {
+function no(question) {
+  let validResponse = false;
+  while(!validResponse) {
+    let response = prompt(question);
+    if(response !== null) {
+      response = response.toLowerCase();
       if(response === 'no' || response === 'n') {
         numberCorrect++;
         alert('Good job, that\'s correct! You have answered ' + numberCorrect + ' questions correctly!');
@@ -37,17 +36,18 @@ function no(response) {
         alert('Invalid input, plese try "Yes", "No", "Y" or "N"');
         // console.log('Invalid input, plese try "Yes", "No", "Y" or "N"')
       }
+    } else {
+      validResponse = true;
     }
   }
 }
 
-function yes(response) {
-  if(response === null) {
-    response = '';
-  } else {
-    response = response.toLowerCase();
-    let validResponse = false;
-    while(!validResponse) {
+function yes(question) {
+  let validResponse = false;
+  while(!validResponse) {
+    let response = prompt(question);
+    if(response !== null) {
+      response = response.toLowerCase();
       if(response === 'yes' || response === 'y') {
         numberCorrect++;
         alert('Good job, that\'s correct! You have answered ' + numberCorrect + ' questions correctly!');
@@ -61,6 +61,8 @@ function yes(response) {
         alert('Invalid input, plese try "Yes", "No", "Y" or "N"');
         // console.log('Invalid input, plese try "Yes", "No", "Y" or "N"')
       }
+    } else {
+      validResponse = true;
     }
   }
 }

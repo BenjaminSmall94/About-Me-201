7997ca2f6a8d53ec32a5d519690f9ab77743b024
Benjamin Small <smallbenjamin94@gmail.com>
2022-04-08 10:46:05 -0700

    refactored code for questions into arrays and for loops

--- js/app.js
+++ js/app.js
@@ -1,17 +1,31 @@
 'use strict';
 
+let username = getUsername();
+let numberCorrect = 0;
+document.getElementById('quizzer').addEventListener('click', doQuiz);
+
+function getUsername() {
+  let user = prompt('What is your name?');
+  document.getElementById('name').textContent = user;
+  alert('Greetings ' + user + ', welcome to the Benjamin Small immersive experience\r\rIf you can answer these simple questions correct we will know you are a true Benjain Small superfan and will be entered into a raffle to win our grand prize of $100 million!!\r\rTry clicking INTERACTIVE');
+  return user;
+}
+
 function doQuiz() {
+  let yesNoQs = ['Was Ben born on a Thursday?', 'Has Ben ever worked for Miracle Ram?', 'Does Ben want kids?', 'Is Ben an only child?', 'Does Ben have more Nephews than Neices?'];
+  for(let i = 0; i < yesNoQs.length; i++) {
+    yesNo(yesNoQs[i], i % 2 !== 1);
+  }
   let favoriteHobbies = ['Skiing', 'Disc Golf', 'Ping Pong', 'Board Games', 'Hiking'];
-  firstFiveQs();
   guessRandomNumber();
   let hobbyResults = guessHobby(6, favoriteHobbies);
-  printHobbyResults(hobbyResults[0], hobbyResults[1]);
+  printHobbyResults(favoriteHobbies, hobbyResults[0], hobbyResults[1]);
   giveResults();
 }
 
 function giveResults() {
   if(numberCorrect >= 7) {
-    alert('Congatulations ' + username + '! You are a true Benjamin Small superfan! You answered all ' + numberCorrect + ' questions correctly. None can compare to your prowess!! I know we promised you $100 million, but please take this complementary "Atta boy"!!');
+    alert('Congatulations ' + username + '! You are a true Benjamin Small superfan! You answered all ' + numberCorrect + ' questions correctly. None can compare to your prowess!!\r\rI know we promised you $100 million, but please take this complementary "Atta boy"!!');
   } else if (numberCorrect > 0) {
     alert('Sorry ' + username + ', you only got ' + numberCorrect + ' correct. You are not a true Benjamin Small superfan, from the bottom of my heart... I pity you. :(');
   } else {
@@ -44,14 +58,27 @@ function guessHobby(guessesRemaining, hobbies) {
   return [true, 0];
 }
 
-function printHobbyResults(notGuessed, guessesRemaining) {
-  let favoriteHobbyString = 'Skiing, Disc Golf, Ping Pong, Board Games, or Hiking';
+function printHobbyResults(hobbyArray, notGuessed, guessesRemaining) {
   if(notGuessed) {
-    alert(`You have exhausted all your guesses, correct answers include ${favoriteHobbyString}`);
+    alert(`You have exhausted all your guesses, correct answers include ${arrayToString(hobbyArray)}.`);
   } else if (guessesRemaining === 5) {
-    alert(`YOU ANSWERED CORRECTLY!! It only took you 1 guess. Correct answers include ${favoriteHobbyString}`);
+    alert(`YOU ANSWERED CORRECTLY!! It only took you 1 guess. Correct answers include ${arrayToString(hobbyArray)}.`);
   } else {
-    alert(`YOU ANSWERED CORRECTLY!! It only took you ${6 - guessesRemaining} guesses. Correct answers include ${favoriteHobbyString}.`);
+    alert(`YOU ANSWERED CORRECTLY!! It only took you ${6 - guessesRemaining} guesses. Correct answers include ${arrayToString(hobbyArray)}.`);
+  }
+}
+
+function arrayToString(array) {
+  if (array !== null) {
+    let arrayString = '';
+    for(let i = 0; i < array.length; i++) {
+      if(i === array.length - 1) {
+        arrayString += array[i];
+      } else {
+        arrayString += array[i] + ', ';
+      }
+    }
+    return arrayString;
   }
 }
 
@@ -69,12 +96,13 @@ function guessRandomNumber() {
       return false;
     }
     userGuess = parseInt(userGuess);
-    if(userGuess === randomNumber && guessesRemaining === 4) {
+    guessesRemaining--;
+    if(userGuess === randomNumber && guessesRemaining === 3) {
       alert(`Great Job!! You correctly guessed the number ${randomNumber} in 1 guess!!`);
       numberCorrect++;
       return true;
     } else if (userGuess === randomNumber) {
-      alert(`Great Job!! You correctly guessed the number ${randomNumber} in ${5 - guessesRemaining} guesses!!`);
+      alert(`Great Job!! You correctly guessed the number ${randomNumber} in ${4 - guessesRemaining} guesses!!`);
       numberCorrect++;
       return true;
     } else if (userGuess < randomNumber) {
@@ -84,83 +112,43 @@ function guessRandomNumber() {
     } else {
       alert (`Invalid input, please guess a number.\r\rYou have ${guessesRemaining} guesses remaining`);
     }
-    guessesRemaining--;
   }
   alert('You have exhausted all your guesses, I am disappointed in your guessing skills');
   return false;
 }
 
-function getUsername() {
-  let user = prompt('What is your name?');
-  document.getElementById('name').textContent = user;
-  alert('Greetings ' + user + ', welcome to the Benjamin Small immersive experience\r\rIf you can answer these simple questions correct we will know you are a true Benjain Small superfan and will be entered into a raffle to win our grand prize of $100 million!!\r\rTry Clicking INTERACTIVE');
-  return user;
-}
 
-function firstFiveQs() {
-  no('Was Ben born on a Wednesday?');
-  console.log('Was Ben born on a Wednesday?');
-  no('Has Ben ever worked for Miracle Ram?');
-  console.log('Has Ben ever worked for Miracle Ram?');
-  yes('Does Ben want kids?');
-  console.log('Does Ben want kids?');
-  no('Is Ben an only child?');
-  console.log('Is Ben an only child?');
-  yes('Does Ben have more Nephews than Neices?');
-  console.log('Does Ben have more Nephews than Neices?');
-}
-
-function no(question) {
+function yesNo(question, answer) {
   let validResponse = false;
   while(!validResponse) {
     let response = prompt(question);
     if(response !== null) {
       response = response.toLowerCase();
-      if(response === 'no' || response === 'n') {
-        numberCorrect++;
-        alert('Good job, that\'s correct! You have answered ' + numberCorrect + ' questions correctly!');
-        validResponse = true;
-        // console.log("Good job, that's correct! You have answered " + numberCorrect + " questions correctly!")
-      } else if (response === 'yes' || response === 'y') {
-        alert('That is incorrect. You have answered ' + numberCorrect + ' questions correctly!');
-        validResponse = true;
-        // console.log("That is incorrect.")
+      if(!answer) {
+        if(response === 'no' || response === 'n') {
+          numberCorrect++;
+          alert('Good job, that\'s correct! You have answered ' + numberCorrect + ' questions correctly!');
+          validResponse = true;
+        } else if (response === 'yes' || response === 'y') {
+          alert('That is incorrect. You have answered ' + numberCorrect + ' questions correctly!');
+          validResponse = true;
+        } else {
+          alert('Invalid input, plese try "Yes", "No", "Y" or "N"');
+        }
       } else {
-        alert('Invalid input, plese try "Yes", "No", "Y" or "N"');
-        // console.log('Invalid input, plese try "Yes", "No", "Y" or "N"')
+        if(response === 'yes' || response === 'y') {
+          numberCorrect++;
+          alert('Good job, that\'s correct! You have answered ' + numberCorrect + ' questions correctly!');
+          validResponse = true;
+        } else if (response === 'no' || response === 'n') {
+          alert('That is incorrect. You have answered ' + numberCorrect + ' questions correctly!');
+          validResponse = true;
+        } else {
+          alert('Invalid input, plese try "Yes", "No", "Y" or "N"');
+        }
       }
     } else {
       validResponse = true;
     }
   }
 }
-
-function yes(question) {
-  let validResponse = false;
-  while(!validResponse) {
-    let response = prompt(question);
-    if(response !== null) {
-      response = response.toLowerCase();
-      if(response === 'yes' || response === 'y') {
-        numberCorrect++;
-        alert('Good job, that\'s correct! You have answered ' + numberCorrect + ' questions correctly!');
-        validResponse = true;
-        // console.log("Good job, that's correct! You have answered " + numberCorrect + " questions correctly!")
-      } else if (response === 'no' || response === 'n') {
-        alert('That is incorrect. You have answered ' + numberCorrect + ' questions correctly!');
-        validResponse = true;
-        // console.log("That is incorrect.")
-      } else {
-        alert('Invalid input, plese try "Yes", "No", "Y" or "N"');
-        // console.log('Invalid input, plese try "Yes", "No", "Y" or "N"')
-      }
-    } else {
-      validResponse = true;
-    }
-  }
-}
-
-let username = getUsername();
-let numberCorrect = 0;
-let quizzer = document.getElementById('quizzer');
-quizzer.addEventListener('click', doQuiz);
